5bc9528b4a581388fc16018fba867a8e3042e7fc
venulgs <venu.koratla@labyrinthglobalsolutions.com>
2023-12-06 14:03:12 +0530

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1>Hi guys tomorrow we will start development of XpenseFlow </h1>
-      <h2>Venu forked Repo</h2>
+      <h2>Venu forked Repository</h2>
     </>
   );
 }
